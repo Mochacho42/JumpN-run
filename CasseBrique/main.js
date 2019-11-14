7399a511118d897e00f585cb8a3a2bb14ac8210f
Mohamed CHACHOUR <Mochacho42@users.noreply.github.com>
2019-11-14 16:45:59 +0100

    deplacer le script dans le main.js modification de la taille du canvas

--- CasseBrique/main.js
+++ CasseBrique/main.js
@@ -1,13 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var score = 0;
+var lives = 3;
 // Definir si boutons sont pressés ou pas
 var rightPressed = false;
 var leftPressed = false;
@@ -19,50 +21,57 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
-var gameOverNotify = document.querySelector('.game-over-notify');
-var interval;
-
 var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
-  bricks[c] = [];
-  for(var r=0; r<brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
+for(var c = 0; c < brickColumnCount; c++) {
+bricks[c] = [];
+  for(var r = 0; r < brickRowCount; r++) {
+  bricks[c][r] = {x: 0, y: 0, status: 1};
   }
 }
-// Savoir si touches sont pressées
+// Savoir si touches sont pressées & si souris bouge
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-gameOverNotify.addEventListener("click", function() {
-  document.location.reload();
-});
+document.addEventListener("mousemove", mouseMoveHandler, false);
 // Permet de bouger le rectangle du bas (lorsque les touches sont enfoncées)
 function keyDownHandler(e) {
-  if(e.keyCode == 39) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = true;
   }
-  else if(e.keyCode == 37) {
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = true;
   }
 }
 // Permet de bouger le rectangle du bas (lorsque les touchent sont lachées)
 function keyUpHandler(e) {
-  if(e.keyCode == 39) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = false;
   }
-  else if(e.keyCode == 37) {
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
   }
 }
-// Dessiner la balle
+// Permet de bouger le rectangle du bas (avec la souris)
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+  paddleX = relativeX - paddleWidth/2;
+  }
+}
+// Détecter la collision
 function collisionDetection() {
-  for(var c=0; c<brickColumnCount; c++) {
-    for(var r=0; r<brickRowCount; r++) {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
       var b = bricks[c][r];
-      if(b.status == 1) {
-        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-          dy = -dy;
-          b.status = 0;
+      if (b.status == 1) {
+      if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+        dy = -dy;
+        b.status = 0;
+        score++;
+        if (score == brickRowCount*brickColumnCount) {
+        alert("YOU WIN, CONGRATS!");
+        document.location.reload();
         }
+      }
       }
     }
   }
@@ -75,7 +84,7 @@ function drawBall() {
   ctx.fill();
   ctx.closePath();
 }
-// Dessiner rectangle qui bouge
+// Dessiner le rectangle du bas
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -83,23 +92,35 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
-// Permet de dessiner les briques
+// Dessiner les briques
 function drawBricks() {
-  for(var c=0; c<brickColumnCount; c++) {
-    for(var r=0; r<brickRowCount; r++) {
-      if(bricks[c][r].status == 1) {
-        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+      var brickX = (r * (brickWidth+brickPadding)) + brickOffsetLeft;
+      var brickY = (c * (brickHeight+brickPadding)) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
       }
     }
   }
+}
+// Afficher le score
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: "+score, 8, 20);
+}
+// Afficher les vies restantes
+function drawLives() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 // Permet de tout dessiner
 function draw() {
@@ -107,34 +128,44 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  drawScore();
+  drawLives();
   collisionDetection();
 
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy;
   }
-  else if(y + dy > canvas.height-ballRadius) {
+  else if (y + dy > canvas.height-ballRadius) {
     if(x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     }
     else {
-      gameOverNotify.style.display = 'flex';
-      clearInterval(interval);
-      return;
-    }
+      lives--;
+      if(!lives) {
+      alert("GAME OVER");
+      document.location.reload();
+      }
+      else {
+      x = canvas.width/2;
+      y = canvas.height-30;
+      dx = 3;
+      dy = -3;
+      paddleX = (canvas.width-paddleWidth)/2;
+      }
   }
-
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-  }
-  else if(leftPressed && paddleX > 0) {
-    paddleX -= 7;
-  }
-
-  x += dx;
-  y += dy;
+}
+if (rightPressed && paddleX < canvas.width-paddleWidth) {
+  paddleX += 7;
+}
+else if (leftPressed && paddleX > 0) {
+  paddleX -= 7;
+}
+x += dx;
+y += dy;
+requestAnimationFrame(draw);
 }
 
-interval = setInterval(draw, 10);
+draw();
