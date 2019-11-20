dfd1a61eb0903e90dfaa601e6f6700413fd62242
Leirda01 <adrieldj@orange.fr>
2019-11-20 15:52:32 +0100

    Ajouter un guillement manquant.

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,14 +1,16 @@
-/* Un truc pour gérer la physique d’une manière toute conne.
- * Genre la gravité, les collisions un minimum, et le vrai luxe serait de gérer
- * le temps. On fout tout ça dans un objet ou chépatrokoi, et il peut être
- * « appliqué » sur d’autres objets et définis des méthodes à utiliser ou non
- * pour définir la physique. Je pense que c’est une bonne idée.
- *
- * Je pense que ce n’est pas une bonne idée d’utiliser autre chose que « jump »
- * pour le saut de Pikachu, et avec un micro-système physique, il suffirait de
- * lui appliquer une force en ordonnée suffisante pour contrer la gravité (qui
- * fait tomber les objets en boucle). Un truc comme ça me semble beaucoup plus
- * robuste et résistants aux bugs. Faut le coder.
- *
- * Des bisous et désolé à ceux qui ont pensé que j’avais enfin rajouté du code.
- */
+function Physics(x, y) {
+  this.x = x;
+  this.y = y;
+  this.xVel = 0;
+  this.yVel = 0;
+
+  let setForce = function (x, y) {
+    this.xVel += x;
+    this.yVel += y;
+  }
+
+  let applyForce = function() {
+
+  }
+
+}
